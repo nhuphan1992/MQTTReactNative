0580c168cfe5e29d44532602d6914630b7e35a32
NhuP+ThuP <nhu.phan@eastagile.com>
2018-10-12 14:40:19 +0700

    install redux for project

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return <View />;
+    return <View style={styles} />;
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
