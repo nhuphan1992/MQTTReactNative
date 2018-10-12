09d67adfc2211bdded4b58910d6b4b1299d05004
hp1909 <hoangphuc1909@live.com>
2018-10-12 12:30:39 +0700

    init project and config eslint

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return <View />;
+  }
+}
+
+const styles = StyleSheet.create({});
