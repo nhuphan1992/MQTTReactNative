8cc5028ef8bc577444e5ed0dff1e0fdfbeb8eefe
hp1909 <hoangphuc1909@live.com>
2018-10-14 23:55:27 +0700

    apply redux for app

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return <View style={styles} />;
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={styles} />
+      </Provider>
+    );
   }
 }
 
